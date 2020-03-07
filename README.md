2bb4aef753d62780ca4576a89b500a49e385a8ce
Leandro Cavalcante <contato@leandrw.com.br>
2020-03-07 01:01:17 -0300

    feat: added ps module and manifest

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # windows-theme
-Powershell module to easily switch between Windows 10 Light and Dark themes.
+> Powershell module to easily switch between Windows 10 Light and Dark themes.
+
+## Requirements
+* Windows 10
+* Powershell version 5 or higher or Powershell core
+
+## Install
+
+First, [see the instructions to enable PSGallery repository in PowerShell](https://docs.microsoft.com/en-us/powershell/scripting/gallery/overview), then proceed to `windows-theme` module instalation. 
+
+Open a powershell terminal and enter de following command:
+```powershell
+Install-Module -Name windows-theme
+```
+
+## Usage
+```powershell
+Get-WindowsTheme # show what theme config is in use for system and apps
+win-theme-default # enable default theme (dark bar, light apps). Same as Use-WindowsDefaultTheme
+win-theme-inverted # enable inverted theme (light bar, dark apps). Same as Use-WindowsInvertedTheme
+win-theme-light # enable light theme. Same as Use-WindowsLightTheme
+win-theme-dark # enable dark theme. Same as Use-WindowsDarkTheme
+Set-WindowsTheme # set -System and -Apps theme separately
+Get-WindowsAppsTheme # show theme in use for apps
+Set-WindowsAppsTheme # set theme for apps only
+Get-WindowsSystemTheme # show theme in use for system only
+Set-WindowsSystemTheme # set theme for system only
+```
+
+Possible values for params are: `light` or `dark`.
---
+++ windows-theme.psm1
@@ -0,0 +1,133 @@
+$keyPath = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize'
+$appsThemeKeyName = 'AppsUseLightTheme'
+$systemThemeKeyName = 'SystemUsesLightTheme'
+
+$lightTheme = 'light'
+$darkTheme = 'dark'
+
+Function themeToRegistryValue {
+    Param (
+        [ValidateSet( 'light', 'dark' )]
+        [string] $theme
+    )
+
+    return (($theme -eq $lightTheme) ? 1 : 0)
+}
+
+Function registryValueToTheme {
+    Param (
+        [ValidateSet(0, 1)]
+        [int] $value
+    )
+
+    return (($value -eq 1) ? $lightTheme : $darkTheme)
+}
+
+Function Get-PersonalizeRegistry {
+    Param (
+        [string] $Key
+    )
+
+    return (Get-ItemProperty -Path $keyPath -Name $Key).($Key);    
+}
+
+Function Set-PersonalizeRegistry {
+    Param (
+        [string] $Key,
+        $Value
+    )
+
+    New-ItemProperty -Path $keyPath -Name $Key -Value $Value -Type Dword -Force | out-null
+}
+
+Function Get-WindowsAppsTheme {
+    return (registryValueToTheme (Get-PersonalizeRegistry $appsThemeKeyName))
+}
+
+Function Set-WindowsAppsTheme {
+    Param (
+        [ValidateSet( 'light', 'dark' )]
+        [string] $value
+    )
+
+    Set-PersonalizeRegistry -Key $appsThemeKeyName -Value (themeToRegistryValue $value)
+}
+
+Function Get-WindowsSystemTheme {
+    return (registryValueToTheme (Get-PersonalizeRegistry $systemThemeKeyName))
+}
+
+Function Set-WindowsSystemTheme {
+    Param (
+        [ValidateSet( 'light', 'dark' )]
+        [string] $value
+    )
+
+    Set-PersonalizeRegistry -Key $systemThemeKeyName -Value (themeToRegistryValue $value)    
+}
+
+Function Get-WindowsTheme {
+    [PSCustomObject]@{
+        apps = Get-WindowsAppsTheme
+        system = Get-WindowsSystemTheme
+    }
+}
+
+Function Set-WindowsTheme {
+    Param (
+        [ValidateSet( 'light', 'dark' )]
+        [string] $Apps = $lightTheme,
+        [ValidateSet( 'light', 'dark' )]
+        [string] $System = $darkTheme
+    )
+
+    # apps theme param
+    Set-WindowsAppsTheme $Apps
+
+    # system theme param
+    Set-WindowsSystemTheme $System
+}
+
+Function Use-WindowsDefaultTheme {
+    Set-WindowsTheme
+}
+
+Function Use-WindowsInvertedTheme {
+    Set-WindowsTheme -Apps $darkTheme -System $lightTheme
+}
+
+Function Use-WindowsLightTheme {
+    Set-WindowsTheme -Apps $lightTheme -System $lightTheme
+}
+
+Function Use-WindowsDarkTheme {
+    Set-WindowsTheme -Apps $darkTheme -System $darkTheme
+}
+
+Set-Alias -Name 'win-theme-default' -Value Use-WindowsDefaultTheme 
+Set-Alias -Name 'win-theme-inverted' -Value Use-WindowsInvertedTheme
+Set-Alias -Name 'win-theme-light' -Value Use-WindowsLightTheme
+Set-Alias -Name 'win-theme-dark' -Value Use-WindowsDarkTheme
+
+$moduleMember = @{
+    Function = @(
+        'Get-WindowsTheme',
+        'Set-WindowsTheme',
+        'Get-WindowsAppsTheme',
+        'Set-WindowsAppsTheme',
+        'Get-WindowsSystemTheme',
+        'Set-WindowsSystemTheme',
+        'Use-WindowsDefaultTheme',
+        'Use-WindowsInvertedTheme',
+        'Use-WindowsLightTheme',
+        'Use-WindowsDarkTheme'
+    )
+    Alias = @(
+        'win-theme-default',
+        'win-theme-inverted',
+        'win-theme-light',
+        'win-theme-dark'
+    )
+}
+
+Export-ModuleMember @moduleMember
